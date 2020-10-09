4ac11782f31d9197c0d63bd9b9f3e2456a3d60d2
Floriane Lefebvre <flolefeb@student.42.fr>
2020-10-09 17:53:10 +0200

    cub3d.c

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: flolefeb <flolefeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 11:08:37 by flolefeb          #+#    #+#             */
-/*   Updated: 2020/09/30 12:05:27 by flolefeb         ###   ########.fr       */
+/*   Updated: 2020/10/09 17:52:42 by flolefeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,30 +32,21 @@ int		main(int argc, char **argv)
 	int		i;
 
 	i = 0;
-	if(!(env = malloc(sizeof(t_env))))
+	if (!(env = malloc(sizeof(t_env))))
 		return (print_error(MALLOC_FAILED, env));
 	ft_bzero(env, sizeof(t_env));
 	if ((argc != 2 && argc != 3))
 		return (print_error(WRONG_ARGS, env));
-	
 	if ((env->error = treat_file(argv[1], env)) != SUCCESS)
 		return (print_error(env->error, env));
-	
 	if ((env->error = init_raycasting(env)) != SUCCESS)
 		return (print_error(env->error, env));
 	if (argc == 3 && (env->error = check_flag_save(argv[2], env)) != SUCCESS)
 		return (print_error(env->error, env));
-	if (env->save == 1)
-	{
-		if ((env->error = save_bmp(env)) == SUCCESS)
-			return (SUCCESS);
-		else
-			return (print_error(env->error, env));
-	}
-	
+	if (env->save == 1 && ((env->error = save_bmp(env)) != SUCCESS))
+		return (print_error(env->error, env));
 	else if ((env->error = raycasting(env)) != SUCCESS)
 		return (print_error(env->error, env));
-	
 	quit(env);
 	return (1);
 }
